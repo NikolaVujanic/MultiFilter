2429b491ea08daa38d8fc0665be26ae444ba0c47
Nikola Vujanic <nikolavuja91@gmail.com>
2015-10-04 04:40:22 +0200

    adding styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+'use strict';
 
+toggleCheckbox();
+
+function toggleCheckbox() {
+    var filters = document.querySelectorAll("input[type=checkbox]");
+    for (var i=0; i < filters.length; i++) {
+        filters[i].addEventListener('click', function() {
+            this.nextElementSibling.children[0].classList.toggle('active');
+        })
+    }
+}
